b1d0082e3c3a86118da0b331391c372f7df10537
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-07 11:09:06 -0600

    typo fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./api/server.js");
 
 //heroku will add the PORT var to the enviornment
-const port = process.enc.PORT || 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
